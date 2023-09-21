c37dd21a48440c30c4f2cfee6c59a000d942356d
Gia Khang <45289938+giakhang3005@users.noreply.github.com>
2023-09-21 21:13:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home/Home.js";
 import { Login } from "./pages/Login/Login";
 import { Navbar } from "./components/Navbar/Navbar";
 import {CreatePost} from "./pages/CreatePost/CreatePost.js"
+import { NotFound404 } from "./pages/NotFound404"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/createpost" element={<CreatePost />} />
+          <Route path="/*" element={<NotFound404 />} />
         </Routes>
       </BrowserRouter>
     </div>
