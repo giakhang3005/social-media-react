f7be4d45c8ad0fd8b4f240b11fae9102da7b9617
Gia Khang <45289938+giakhang3005@users.noreply.github.com>
2023-09-21 21:12:42 +0700

    Add files via upload

---
+++ src/pages/NotFound404.js
@@ -0,0 +1,9 @@
+import {Empty} from 'antd';
+
+export const NotFound404 = () => {
+    return (
+    <>
+        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="404 Not found"> </Empty> 
+    </>
+    )
+}
